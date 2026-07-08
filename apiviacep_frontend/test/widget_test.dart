802d4a7eacab5722796fe7a8dfe71c7838c507fb
Edelson Bessa <edelsonbessa@gmail.com>
2026-07-08 19:01:19 -0300

    Adiciona estrutura básica do aplicativo e campos de entrada para consulta de endereço

--- apiviacep_frontend/test/widget_test.dart
+++ apiviacep_frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:apiviacep_frontend/main.dart';
+import 'package:apiviacep_frontend/apiviacep_views/apiviacep_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
